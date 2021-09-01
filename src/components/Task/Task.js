aca55d0680c04495f3919c7fc193812733164722
19520032 <cuongsdbt@gmail.com>
2021-09-01 10:50:13 +0700

    Clean React Architecture

---
+++ src/components/Task/Task.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Task.scss'
+
+function Task() {
+    return (
+        <li className="task-item">
+            <img src="https://scontent-xsp1-3.xx.fbcdn.net/v/t1.6435-9/186476445_2851274748473229_1833304691370865343_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=iTZ3D_ogQqoAX8MpZmK&_nc_ht=scontent-xsp1-3.xx&oh=37d2ede5322aab5faa7d995fd56dc5b6&oe=61527C85"
+                alt="minhcuongdev-alt-img" />
+            Title: MNHCUONGDEV
+        </li>
+    )
+}
+
+export default Task;
