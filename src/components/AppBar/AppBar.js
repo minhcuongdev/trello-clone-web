57b2a17355177c5f33c7f2c28ab8fc42bb0f2113
19520032 <cuongsdbt@gmail.com>
2021-10-11 14:16:51 +0700

    design board bar

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,9 +1,51 @@
 import React from 'react'
 import './AppBar.scss'
+import { Container as BootstrapContainer, Row, Col, InputGroup, FormControl } from 'react-bootstrap'
 
 function AppBar() {
     return (
-        <nav className="navbar-app">App Bar</nav>
+        <nav className="navbar-app">
+            <BootstrapContainer className="Navbar-container">
+                <Row>
+                    <Col sm={4} xs={6} className="Col-no-padding">
+                        <div className="App-actions">
+                            <div className="item all"><i className="fa fa-th" /></div>
+                            <div className="item home"><i className="fa fa-home" /></div>
+                            <div className="item boards"><i className="fa fa-columns" /></div>
+                            <div className="item search">
+                                <InputGroup className="group-search">
+                                    <FormControl
+                                        className="input-search"
+                                        placeholder="Jump to..."
+                                    />
+                                    <InputGroup.Text className="input-icon-search"><i className="fa fa-search"></i></InputGroup.Text>
+                                </InputGroup>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col sm={4} xs={1} className="Col-no-padding center">
+                        <div className="app-branch text-center">
+                            <a href="https://www.facebook.com/minhcuong.tran.374" target="_blank" rel="noreferrer">
+                                <span className="slogan">minhcuongdev</span>
+                            </a>
+                        </div>
+                    </Col>
+                    <Col sm={4} xs={5} className="Col-no-padding">
+                        <div className="user-actions">
+                            <div className="item quick"><i className="fa fa-plus-square-o" /></div>
+                            <div className="item news"><i className="fa fa-info-circle" /></div>
+                            <div className="item notification"><i className="far fa-bell"></i></div>
+                            <div className="item user-avatar">
+                                <img src="https://i.ibb.co/4pfzCQt/minhcuongdev.jpg"
+                                    alt="minhcuongdev"
+                                    title="minhcuongdev"
+                                />
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </BootstrapContainer>
+        </nav>
     )
 }
 
--- src/components/BoardBar/BoardBar.js
+++ src/components/BoardBar/BoardBar.js
@@ -1,9 +1,42 @@
 import React from 'react'
 import './BoardBar.scss'
+import { Container as BootstrapContainer, Row, Col } from 'react-bootstrap'
 
 function BoardBar() {
     return (
-        <nav className="navbar-board">Board Bar</nav>
+        <nav className="navbar-board">
+            <BootstrapContainer className="Container">
+                <Row>
+                    <Col sm={8} xs={12} className="col-no-padding">
+                        <div className="board-info">
+                            <div className="item board-logo-icon">
+                                <i className="fa fa-coffee"></i>
+                                &nbsp;&nbsp;<strong>minhcuongdev REACTJS</strong>
+                            </div>
+                            <div className="divider"></div>
+
+                            <div className="item board-type">Private Workspace</div>
+                            <div className="divider"></div>
+
+                            <div className="item member-avatar">
+                                <img src="https://i.ibb.co/4pfzCQt/minhcuongdev.jpg"
+                                    alt="minhcuongdev"
+                                    title="minhcuongdev"
+                                />
+                                <span className="more-members">+1</span>
+                                <span className="invite">Invite</span>
+                            </div>
+
+                        </div>
+                    </Col>
+                    <Col className="col-no-padding" sm={4} xs={12}>
+                        <div className="board-actions">
+                            <div className="item menu"><i className="fa fa-ellipsis-h mr-2"></i>Show menu</div>
+                        </div>
+                    </Col>
+                </Row>
+            </BootstrapContainer>
+        </nav>
     )
 }
 
